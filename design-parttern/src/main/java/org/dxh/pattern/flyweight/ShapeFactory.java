11cd789ffe992c509b188392375363bd70b13ca7
xiaohujapan <xiaohujapan@yahoo.co.jp>
2019-02-19 16:49:14 +0900

    【享元模式（Flyweight Pattern）（フライウェイト・パターン）】

---
+++ design-parttern/src/main/java/org/dxh/pattern/flyweight/ShapeFactory.java
@@ -0,0 +1,20 @@
+package org.dxh.pattern.flyweight;
+
+import java.util.HashMap;
+
+import org.dxh.pattern.factory.Shape.Shape;
+
+public class ShapeFactory {
+	 private static final HashMap<String, Shape> circleMap = new HashMap<>();
+	   
+	 public static Shape getCircle(String color) {
+	    Circle circle = (Circle)circleMap.get(color);
+	 
+	    if(circle == null) {
+	       circle = new Circle(color);
+	       circleMap.put(color, circle);
+	       System.out.println("Creating circle of color : " + color);
+	    }
+	    return circle;
+	}
+}
