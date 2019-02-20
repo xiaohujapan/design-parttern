829e5ecd192d0f2bfb5568f9ba402e46c9bd4e9e
xiaohujapan <xiaohujapan@yahoo.co.jp>
2019-02-20 15:54:41 +0900

    责任链模式（Chain of Responsibility Pattern）

---
+++ design-parttern/src/main/java/org/dxh/pattern/behavioral/chain/ErrorLogger.java
@@ -0,0 +1,14 @@
+package org.dxh.pattern.behavioral.chain;
+
+public class ErrorLogger extends AbstractLogger {
+
+	public ErrorLogger(int level){
+	    this.level = level;
+	}
+	
+	@Override
+	protected void write(String message) {
+		System.out.println("Error Console::Logger: " + message);
+	}
+
+}
