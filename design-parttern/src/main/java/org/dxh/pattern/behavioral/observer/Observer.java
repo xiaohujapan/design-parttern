c59d690e9ba5330fce05a74635b9ccf0d9c1859e
xiaohujapan <xiaohujapan@yahoo.co.jp>
2019-02-21 11:05:21 +0900

    【观察者模式（Observer Pattern）（オブザーバ・パターン）】

---
+++ design-parttern/src/main/java/org/dxh/pattern/behavioral/observer/Observer.java
@@ -0,0 +1,6 @@
+package org.dxh.pattern.behavioral.observer;
+
+public abstract class Observer {
+	   protected Subject subject;
+	   public abstract void update();
+}
---
+++ design-parttern/src/main/java/org/dxh/pattern/behavioral/observer/Subject.java
@@ -0,0 +1,29 @@
+package org.dxh.pattern.behavioral.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+	private List<Observer> observerList = new ArrayList<Observer>();
+	
+	private int state;
+
+	public int getState() {
+	   return state;
+	}
+
+	public void setState(int state) {
+	   this.state = state;
+	   notifyAllObservers();
+	}
+
+	public void attach(Observer observer){
+		observerList.add(observer);      
+	}
+
+	public void notifyAllObservers(){
+		for (Observer observer : observerList) {
+			observer.update();
+		}
+	}  
+}
