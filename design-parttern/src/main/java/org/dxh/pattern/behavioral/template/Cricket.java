90ad328f82a1cf03d3a9b9c382b862dd6b429cd0
xiaohujapan <xiaohujapan@yahoo.co.jp>
2019-02-21 11:58:00 +0900

    【模板模式（Template Pattern）（テンプレート・メソッド・パターン）】

---
+++ design-parttern/src/main/java/org/dxh/pattern/behavioral/template/Cricket.java
@@ -0,0 +1,20 @@
+package org.dxh.pattern.behavioral.template;
+
+public class Cricket extends Game {
+
+	@Override
+	void initialize() {
+		System.out.println("Cricket Game Finished!");
+	}
+
+	@Override
+	void startPlay() {
+		System.out.println("Cricket Game Initialized! Start playing.");
+	}
+
+	@Override
+	void endPlay() {
+		System.out.println("Cricket Game Started. Enjoy the game!");
+	}
+
+}
---
+++ design-parttern/src/main/java/org/dxh/pattern/behavioral/template/Football.java
@@ -0,0 +1,20 @@
+package org.dxh.pattern.behavioral.template;
+
+public class Football extends Game {
+
+	@Override
+	void initialize() {
+		System.out.println("Football Game Finished!");
+	}
+
+	@Override
+	void startPlay() {
+		System.out.println("Football Game Initialized! Start playing.");
+	}
+
+	@Override
+	void endPlay() {
+		System.out.println("Football Game Started. Enjoy the game!");
+	}
+
+}
